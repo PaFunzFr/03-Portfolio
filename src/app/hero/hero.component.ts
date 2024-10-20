a67bca049f2c16bfdd7019c8c70b9c4b68ec0d27
funZ <153361103+PaFunzFr@users.noreply.github.com>
2024-10-20 17:03:42 +0200

    Update 241020

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
+
+  leftClip: number = 50;
+  rightClip: number = 50;
+  transformX: number = 0;
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const positionX = event.clientX; 
+    const windowWidth = window.innerWidth; 
+
+    const mouseXPercentage = (positionX / windowWidth) * 100;
+
+    if (mouseXPercentage < 50) {
+        this.leftClip = mouseXPercentage; 
+        this.rightClip = 100 - this.leftClip;
+        this.transformX = mouseXPercentage * -0.25;
+    } else {
+        this.rightClip = 100 - mouseXPercentage;
+        this.leftClip = 100 - this.rightClip;
+        this.transformX = mouseXPercentage * -0.25;
+    }
+  }
+
+  @HostListener('mouseleave', ['$event'])
+  onMouseLeave() {
+    this.resetClips();
+  }
+
+  resetClips() {
+    this.leftClip = 50;
+    this.rightClip = 50;
+    this.transformX = 0;
+  }
 
 }
