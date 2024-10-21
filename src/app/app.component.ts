ae4599640142fce6891e0ca0c56b1f998046170a
funZ <153361103+PaFunzFr@users.noreply.github.com>
2024-10-21 17:44:50 +0200

    Update 241021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,11 @@ import { Component, HostListener } from '@angular/core';
 export class AppComponent {
   title = '03_Portfolio';
   
-  aboutHeight: number = 0; // height : 0;
+  aboutHeight: number = 0; 
+  aboutOpacity: number = 0;
+  aboutScale: number = 1;
+  aboutBlur: number = 80;
+  mouseOpacity: number = 1;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
@@ -18,6 +22,13 @@ export class AppComponent {
     // calc new height (vh)
     // 100vh = 100% of viewport height
     this.aboutHeight = Math.min(scrollY / viewportHeight * 100, 100);
+    this.aboutOpacity = scrollY / 3000;
+    this.aboutScale = Math.min(scrollY /20 * 0.01, 1);
+    this.aboutBlur = Math.max(80 - (scrollY / 200) * 10, 0);
+
+    if (scrollY > 100) {
+        this.mouseOpacity = 0;
+    }
     // math.min = never bigger than 100 (max height = 100vh)
   }
 }
