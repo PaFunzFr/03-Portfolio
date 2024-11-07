4bb8f818630927a5f4cccd13a62f139ebb4af599
funZ <153361103+PaFunzFr@users.noreply.github.com>
2024-11-07 15:22:33 +0100

    Update 241107

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -7,24 +7,24 @@ import { Component, Input } from '@angular/core';
 })
 export class ProjectCardComponent {
 
+  // project Input
   @Input() projectImage: string = '';
   @Input() projectTitle: string = '';
-
   @Input() projectCode: string = '';
   @Input() projectDemo: string = '';
 
+  // usedTools Input
   @Input() usedTool: string = '';
-
   @Input() usedToolsCount: string = '';
 
-  @Input() displayContent: string = '0';
+  // Hover Show project-infos
+  displayContent: string = '0';
 
   showProjectContent() {
     this.displayContent = this.displayContent = '1';
-  }
-
+  };
   hideProjectContent() {
     this.displayContent = this.displayContent = '0';
-  }
+  };
 
 }
