a6846d1bf66707feaa244c79bf97ae2d11d89af6
funZ <153361103+PaFunzFr@users.noreply.github.com>
2024-11-20 15:06:21 +0100

    Update 241120

--- src/app/components/hobbies/hobbies.component.ts
+++ src/app/components/hobbies/hobbies.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './hobbies.component.scss'
 })
 export class HobbiesComponent {
-  numbers = Array.from({ length: 4 }, (_, i) => i);
 
   factsPictures = [
     '/assets/img/facts/1.jpg',
     '/assets/img/facts/2.jpg',
-    '/assets/img/facts/3.jpg',
-    '/assets/img/facts/4.jpg',
+    '/assets/img/facts/3.jpg'
   ];
+
+
 }
 
