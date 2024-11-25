f607ea796b950870c0f084c5348ad783d65db56e
funZ <153361103+PaFunzFr@users.noreply.github.com>
2024-11-25 15:12:13 +0100

    Update 241125

--- src/app/components/hobbies/hobbies.component.ts
+++ src/app/components/hobbies/hobbies.component.ts
@@ -1,29 +1,119 @@
 import { Component } from '@angular/core';
 
+interface Hobby {
+  name: string;
+  pictures: string[];
+  icon: string;
+}
 @Component({
   selector: 'app-hobbies',
   templateUrl: './hobbies.component.html',
   styleUrl: './hobbies.component.scss',
 })
 export class HobbiesComponent {
-  hobbyPictures = [
-    '/assets/img/facts/1.jpg',
-    '/assets/img/facts/2.jpg',
-    '/assets/img/facts/3.jpg',
+  hobbyPictures: Hobby[] = [
+    {
+      name: 'photography',
+      pictures: [
+        '/assets/img/hobbies/photography/1.jpg',
+        '/assets/img/hobbies/photography/2.jpg',
+        '/assets/img/hobbies/photography/3.jpg',
+      ],
+      icon: '/assets/img/hobbies/photography.svg',
+    },
+    {
+      name: 'cycling',
+      pictures: [
+        '/assets/img/hobbies/cycling/1.jpg',
+        '/assets/img/hobbies/cycling/2.jpg',
+        '/assets/img/hobbies/cycling/3.jpg',
+      ],
+      icon: '/assets/img/hobbies/cycling.svg',
+    },
+    {
+      name: 'dancing',
+      pictures: [
+        '/assets/img/hobbies/dancing/1.jpg',
+        '/assets/img/hobbies/dancing/2.jpg',
+        '/assets/img/hobbies/dancing/3.jpg',
+      ],
+      icon: '/assets/img/hobbies/dancing.svg',
+    },
+    {
+      name: 'metal',
+      pictures: [
+        '/assets/img/hobbies/metal/1.jpg',
+        '/assets/img/hobbies/metal/2.jpg',
+        '/assets/img/hobbies/metal/3.jpg',
+      ],
+      icon: '/assets/img/hobbies/metal.svg',
+    },
+    {
+      name: 'hiking',
+      pictures: [
+        '/assets/img/hobbies/hiking/1.jpg',
+        '/assets/img/hobbies/hiking/2.jpg',
+        '/assets/img/hobbies/hiking/3.jpg',
+      ],
+      icon: '/assets/img/hobbies/hiking.svg',
+    },
+    {
+      name: 'gaming',
+      pictures: [
+        '/assets/img/hobbies/gaming/1.jpg',
+        '/assets/img/hobbies/gaming/2.jpg',
+        '/assets/img/hobbies/gaming/3.jpg',
+      ],
+      icon: '/assets/img/hobbies/gaming.svg',
+    },
   ];
 
-  hobbyIcons = [
-    '/assets/img/hobbies/photography.svg',
-    '/assets/img/hobbies/cycling.svg',
-    '/assets/img/hobbies/dancing.svg',
-    '/assets/img/hobbies/metal.svg',
-    '/assets/img/hobbies/hiking.svg',
-    '/assets/img/hobbies/gaming.svg',
+  /* configured images as inital pictures
+  initialPictures: string[] = [
+    this.hobbyPictures[1].pictures[0],
+    this.hobbyPictures[0].pictures[1],
+    this.hobbyPictures[4].pictures[1],
   ];
+*/
 
+  // randomize initial pictures
+  getRandomUniquePictures(): string[] {
+    const allPictures = this.hobbyPictures.flatMap((hobby) => hobby.pictures); // Alle Bilder in eine flache Liste packen
+    const selectedPictures: Set<string> = new Set();
+
+    while (selectedPictures.size < 3) {
+      const randomIndex = Math.floor(Math.random() * allPictures.length);
+      selectedPictures.add(allPictures[randomIndex]);
+    }
+
+    return Array.from(selectedPictures); // Set in ein Array umwandeln
+  }
+
+  initialPictures: string[] = this.getRandomUniquePictures();
+
+  // define button(s) and its function
   clickedButton: string | null = null; // save active item
 
+  getPicturesByName(hobbyName: string): string[] {
+    const hobby = this.hobbyPictures.find((item) => item.name === hobbyName);
+    return hobby ? hobby.pictures : []; // if no pictures return empty array
+  }
+
+  // picture array definition
+  photographyPictures = this.getPicturesByName('photography');
+  cyclingPictures = this.getPicturesByName('cycling');
+  dancingPictures = this.getPicturesByName('dancing');
+  metalPictures = this.getPicturesByName('metal');
+  hikingPictures = this.getPicturesByName('hiking');
+  gamingPictures = this.getPicturesByName('gaming');
+
+  // click event
   setActive(item: string) {
-    this.clickedButton = this.clickedButton === item ? null : item; // active or not? if not => set active
+    this.clickedButton = item;
+  }
+  get activePictures(): string[] {
+    return this.clickedButton
+      ? this.getPicturesByName(this.clickedButton)
+      : this.initialPictures;
   }
 }
